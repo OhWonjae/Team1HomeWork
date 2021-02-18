0a9c14bca39cb75de2c7df266ada1f782c2ab3b8
kimhyeongyun <ske011217@gmail.com>
2021-02-19 00:58:16 +0900

    형윤 제출

--- src/Team1/KimHyeongYun.java
+++ src/Team1/KimHyeongYun.java
@@ -10,8 +10,7 @@ public class KimHyeongYun {
 		boolean run = true;
 		int sentenceNum = 1;
 		int choiceNum = 0;
-		
-
+	
 		String[][] boardArray = new String[100][5];
 		Scanner scanner = new Scanner(System.in);
 		String s = null;
@@ -26,6 +25,11 @@ public class KimHyeongYun {
 
 
 			int selectNo = Integer.parseInt(scanner.nextLine());
+//			
+//			if(scanner.nextLine() == "") {
+//				continue;
+//			}
+			
 			if(selectNo == 1) { // 1을 읽었을 경우		
 
 				System.out.println("----------------------------------------------------------------------------------");
@@ -40,18 +44,43 @@ public class KimHyeongYun {
 
 			} else if (selectNo == 2) { // 2. 생성(Create)
 				
+				for(int i = 99; i > 0 ;i--) {
+					
+					if(boardArray[i][0] == null && boardArray[i][1] == null && boardArray[i][2] == null && boardArray[i][3] == null && boardArray[i][4] == null) {
+						
+//						boardArray[i][0] = "" + (sentenceNum);
+//						System.out.print("제목: ");
+//						boardArray[i][1] = scanner.nextLine();
+//						System.out.print("내용: ");
+//						boardArray[i][2] = scanner.nextLine();
+//						System.out.print("글쓴이: ");
+//						boardArray[i][3] = scanner.nextLine();
+//		
+//						boardArray[i][4] = "" + 0;
+//					
+//						sentenceNum++;
+						
+					} else {
+						boardArray[100-sentenceNum][0] = "" + (sentenceNum);
+						System.out.print("제목: ");
+						boardArray[100-sentenceNum][1] = scanner.nextLine();
+						System.out.print("내용: ");
+						boardArray[100-sentenceNum][2] = scanner.nextLine();
+						System.out.print("글쓴이: ");
+						boardArray[100-sentenceNum][3] = scanner.nextLine();
+		
+						boardArray[100-sentenceNum][4] = "" + 0;
+					
+						sentenceNum++;
 				
-				boardArray[100-sentenceNum][0] = "" + (sentenceNum);
-				System.out.print("제목: ");
-				boardArray[100-sentenceNum][1] = scanner.nextLine();
-				System.out.print("내용: ");
-				boardArray[100-sentenceNum][2] = scanner.nextLine();
-				System.out.print("글쓴이: ");
-				boardArray[100-sentenceNum][3] = scanner.nextLine();
-
-				boardArray[100-sentenceNum][4] = "" + 0;
-
-				sentenceNum++;
+					}
+				}
+				
+			
+//			if(scanner.nextLine() == "") {
+//				continue;
+//			}
+//
 
 
 
@@ -62,29 +91,40 @@ public class KimHyeongYun {
 
 				choiceNum = Integer.parseInt(scanner.nextLine());
 
+				
+				int views = Integer.parseInt(boardArray[100-choiceNum][4]);
+				views++;
+				boardArray[100-choiceNum][4] = ""+views;
+					
 				System.out.println("번호: "  + boardArray[100-choiceNum][0]);	
 				System.out.println("제목: "  + boardArray[100-choiceNum][1]);
 				System.out.println("내용: "  + boardArray[100-choiceNum][2]);
 				System.out.println("글쓴이: "  + boardArray[100-choiceNum][3]);
+				System.out.println("조회수: "  + boardArray[100-choiceNum][4]);
+				
+				if(scanner.nextLine() == "") {
+					continue;
+				}
 
-				int a = 1;
 				
 				// System.out.println("조회수: " + );
 
-				a++;
-
-				//Integer.scanner.nextInt();
-
+				
 			} else if (selectNo == 4) { // 4. 수정(Update)
 				System.out.print("번호선택> ");
 
 				choiceNum = Integer.parseInt(scanner.nextLine());
 
-				System.out.println("기존제목: " + boardArray[sentenceNum][1]);
+				System.out.println("기존제목: " + boardArray[100-choiceNum][1]);
 				String pretitle = boardArray[sentenceNum][1];
 
 				System.out.println("수정제목: "); 
-				s = scanner.nextLine();
+				scanner.nextLine();
+				
+				if(scanner.nextLine() == "") {
+					continue;
+				}
+				
 				boardArray[sentenceNum][1] = scanner.nextLine();
 
 				if(s.equals(""))
@@ -114,23 +154,34 @@ public class KimHyeongYun {
 
 			} else if (selectNo == 5) { // 5. 삭제(Delete)
 				System.out.print("번호선택> ");
-				selectNo = Integer.parseInt(scanner.nextLine());
-				for(int i=selectNo; i < selectNo + 1; i++) {
-					// boardArray[selectNum][4] = "" ;
-					boardArray[i][0] = null;	
-					boardArray[i][1] = null;
-					boardArray[i][2] = null;
-					boardArray[i][3] = null;
-					boardArray[i][4] = null;
-				}
+				choiceNum = Integer.parseInt(scanner.nextLine());
+				
+				boardArray[choiceNum][0] = null;	
+				boardArray[choiceNum][1] = null;
+				boardArray[choiceNum][2] = null;
+				boardArray[choiceNum][3] = null;
+				boardArray[choiceNum][4] = null;
+				
 
 			} else if (selectNo == 6) { // 6. 종료
 				run = false;
 			}
 
 		}
-
+		
 		System.out.println("프로그램 종료");
+//		int[] scores1 = {90, 85, 95};
+//		int[] scores2 = null;
+//		int[] scores2 = null;
+//		add(scores2); // call by reference
+//		add(scores1);
+//		add(new int[] {100, 200, 300}); // 이거 왜 안되는 이유? 그래서 new int[] 이거를 넣어줘야함.
+//	}
+//	
+//	// method 선언
+//	public static void add(int[] arr) { // add method라 부름
+//		System.out.println("값:" + Arrays.toString(arr)); // Arrays.toString(arr)대신 arr을 출력하면 번지가 나옴. 완전 이상한 값이 나옴.
+//	}
 	}
 
 }
