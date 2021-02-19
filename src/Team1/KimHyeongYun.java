55e3507a65a265d16c1db935e990e80c6ba0e9dd
kimhyeongyun <ske011217@gmail.com>
2021-02-19 21:28:21 +0900

    형윤 제출

--- src/Team1/KimHyeongYun.java
+++ src/Team1/KimHyeongYun.java
@@ -9,20 +9,17 @@ public class KimHyeongYun {
 	public static void main(String[] args) {
 		boolean run = true;
 		int sentenceNum = 1;
-		int choiceNum = 0;
+		int readChoiceNum = 0;
 	
 		String[][] boardArray = new String[100][5];
 		Scanner scanner = new Scanner(System.in);
-		String s = null;
-
+		
 		while(run) {
 
 			System.out.println("----------------------------------------------------------------------------------");
 			System.out.println("1. 목록 | 2. 생성(Create) | 3. 읽기(Read) | 4. 수정(Update) | 5.삭제(Delete) | 6.종료");
 			System.out.println("----------------------------------------------------------------------------------");
 			System.out.print("메뉴선택> ");
-
-
 
 			int selectNo = Integer.parseInt(scanner.nextLine());
 //			
@@ -32,80 +29,48 @@ public class KimHyeongYun {
 			
 			if(selectNo == 1) { // 1을 읽었을 경우		
 
-				System.out.println("----------------------------------------------------------------------------------");
-				System.out.println("번호         제목                내용              글쓴이            조회수");
-				System.out.println("----------------------------------------------------------------------------------");
+				System.out.println("-----------------------------------------------");
+				System.out.println("번호       제목       내용       글쓴이       조회수 ");
+				System.out.println("-----------------------------------------------");
 
 
 				for(int i = 100-sentenceNum + 1 ; i < 100 ; i++) {
 
-					System.out.println(" " + boardArray[i][0] + "         " + boardArray[i][1] + "                   " + boardArray[i][2] + "                     " + boardArray[i][3] + "        " +  boardArray[i][4] );
+					System.out.println(" " + boardArray[i][0] + "       " + boardArray[i][1] + "       " + boardArray[i][2] + "       " + boardArray[i][3] + "           " +  boardArray[i][4] );
 				}
 
 			} else if (selectNo == 2) { // 2. 생성(Create)
 				
-				for(int i = 99; i > 0 ;i--) {
-					
-					if(boardArray[i][0] == null && boardArray[i][1] == null && boardArray[i][2] == null && boardArray[i][3] == null && boardArray[i][4] == null) {
-						
-//						boardArray[i][0] = "" + (sentenceNum);
-//						System.out.print("제목: ");
-//						boardArray[i][1] = scanner.nextLine();
-//						System.out.print("내용: ");
-//						boardArray[i][2] = scanner.nextLine();
-//						System.out.print("글쓴이: ");
-//						boardArray[i][3] = scanner.nextLine();
-//		
-//						boardArray[i][4] = "" + 0;
-//					
-//						sentenceNum++;
-						
-					} else {
-						boardArray[100-sentenceNum][0] = "" + (sentenceNum);
-						System.out.print("제목: ");
-						boardArray[100-sentenceNum][1] = scanner.nextLine();
-						System.out.print("내용: ");
-						boardArray[100-sentenceNum][2] = scanner.nextLine();
-						System.out.print("글쓴이: ");
-						boardArray[100-sentenceNum][3] = scanner.nextLine();
-		
-						boardArray[100-sentenceNum][4] = "" + 0;
-					
-						sentenceNum++;
-				
-					}
-				}
-				
-			
-//			if(scanner.nextLine() == "") {
-//				continue;
-//			}
-//
-
-
-
+				boardArray[100-sentenceNum][0] = "" + sentenceNum;
+				System.out.print("제목: ");
+				boardArray[100-sentenceNum][1] = scanner.nextLine();
+				System.out.print("내용: ");
+				boardArray[100-sentenceNum][2] = scanner.nextLine();
+				System.out.print("글쓴이: ");
+				boardArray[100-sentenceNum][3] = scanner.nextLine();
+	
+				boardArray[100-sentenceNum][4] = "" + 0;
+	
+				sentenceNum++;
 
 			} else if (selectNo == 3) { // 3. 읽기(Read)
 
 				System.out.print("번호선택> ");
 
-				choiceNum = Integer.parseInt(scanner.nextLine());
+				readChoiceNum = Integer.parseInt(scanner.nextLine());
 
 				
-				int views = Integer.parseInt(boardArray[100-choiceNum][4]);
+				int views = Integer.parseInt(boardArray[100-readChoiceNum][4]);
 				views++;
-				boardArray[100-choiceNum][4] = ""+views;
+				boardArray[100-readChoiceNum][4] = ""+views;
 					
-				System.out.println("번호: "  + boardArray[100-choiceNum][0]);	
-				System.out.println("제목: "  + boardArray[100-choiceNum][1]);
-				System.out.println("내용: "  + boardArray[100-choiceNum][2]);
-				System.out.println("글쓴이: "  + boardArray[100-choiceNum][3]);
-				System.out.println("조회수: "  + boardArray[100-choiceNum][4]);
+				System.out.println("번호: "  + boardArray[100-readChoiceNum][0]);	
+				System.out.println("제목: "  + boardArray[100-readChoiceNum][1]);
+				System.out.println("내용: "  + boardArray[100-readChoiceNum][2]);
+				System.out.println("글쓴이: "  + boardArray[100-readChoiceNum][3]);
+				System.out.println("조회수: "  + boardArray[100-readChoiceNum][4]);
 				
-				if(scanner.nextLine() == "") {
-					continue;
-				}
-
+				
 				
 				// System.out.println("조회수: " + );
 
@@ -113,54 +78,43 @@ public class KimHyeongYun {
 			} else if (selectNo == 4) { // 4. 수정(Update)
 				System.out.print("번호선택> ");
 
-				choiceNum = Integer.parseInt(scanner.nextLine());
+				readChoiceNum = Integer.parseInt(scanner.nextLine());
 
-				System.out.println("기존제목: " + boardArray[100-choiceNum][1]);
-				String pretitle = boardArray[sentenceNum][1];
-
-				System.out.println("수정제목: "); 
-				scanner.nextLine();
+				System.out.println("기존제목: " + boardArray[100-readChoiceNum][1]);
+				String preTitle = boardArray[100-readChoiceNum][1];
 				
-				if(scanner.nextLine() == "") {
-					continue;
+				System.out.println("수정제목: "); 
+				String newTitle = scanner.nextLine();
+				
+				if(newTitle.equals("")) {
+					boardArray[100-readChoiceNum][1] = preTitle;
+				} else {
+					boardArray[100-readChoiceNum][1] = newTitle;
 				}
 				
-				boardArray[sentenceNum][1] = scanner.nextLine();
+				
+				
+				System.out.println("기존내용: " + boardArray[100-readChoiceNum][2]);
+				String preContent = boardArray[100-readChoiceNum][2];
 
-				if(s.equals(""))
-				{
-					boardArray[sentenceNum][1] = pretitle;
-				}
-				else
-				{
-					boardArray[sentenceNum][1] = s;
+				System.out.println("수정내용: "); 
+				String newContent= scanner.nextLine();
 
-				}
-				System.out.println("기존제목: " + boardArray[sentenceNum][2]);
-				String precontent = boardArray[sentenceNum][2];
-
-				System.out.println("수정제목: "); 
-				s = scanner.nextLine();
-				boardArray[sentenceNum][2] = scanner.nextLine();
-
-				if(s.equals(""))
-				{
-					boardArray[sentenceNum][2] = precontent;
-				}
-				else
-				{
-					boardArray[sentenceNum][2] = s;
+				if(newContent.equals("")) {
+					boardArray[100-readChoiceNum][2] = preContent;
+				} else {
+					boardArray[100-readChoiceNum][2] = newContent;
 				}
 
 			} else if (selectNo == 5) { // 5. 삭제(Delete)
 				System.out.print("번호선택> ");
-				choiceNum = Integer.parseInt(scanner.nextLine());
+				readChoiceNum = Integer.parseInt(scanner.nextLine());
 				
-				boardArray[choiceNum][0] = null;	
-				boardArray[choiceNum][1] = null;
-				boardArray[choiceNum][2] = null;
-				boardArray[choiceNum][3] = null;
-				boardArray[choiceNum][4] = null;
+				boardArray[100-readChoiceNum][0] = null;	
+				boardArray[100-readChoiceNum][1] = null;
+				boardArray[100-readChoiceNum][2] = null;
+				boardArray[100-readChoiceNum][3] = null;
+				boardArray[100-readChoiceNum][4] = null;
 				
 
 			} else if (selectNo == 6) { // 6. 종료
