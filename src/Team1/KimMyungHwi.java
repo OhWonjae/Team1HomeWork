763b02a92d89a4208e89c669bc62c089956f6ecd
hwee1115 <hwee1115@naver.com>
2021-02-21 02:43:41 +0900

    명휘 수정 제출

--- src/Team1/KimMyungHwi.java
+++ src/Team1/KimMyungHwi.java
@@ -9,12 +9,15 @@ public class KimMyungHwi {
 		String space = " ";
 		boolean run =true;
 		boolean auto=false;
-		int constNum =100;
-		int num=100;
+		//int test=100;
 		int count=100;
 		int choiceNum;
+		//boolean flag=false;
+		//int removeCount=0;
+		int saveRemoveNum=0;
 		String [][] boardArray = new String[100][5];
-
+		//int [] removeArray = new int[100];
+ 
 		Scanner scanner = new Scanner(System.in);
 
 		while(run) {
@@ -31,31 +34,51 @@ public class KimMyungHwi {
 			}
 			//목록
 			if(choiceNum==1) {
+				/*int num=boardArray.length-count;*/
 				System.out.println("--------------------------");
 				System.out.println();
 				System.out.println("번호" +space +  "제목" + space+"내용"+space+"글쓴이"+ space+"조회수");
 				System.out.println();
-				for(int i=0;i<boardArray.length;i++) {
+				for(int i=boardArray.length-1;i>=0;i--) {
 					if(boardArray[i][0]==null)
 						continue;
 					System.out.println(boardArray[i][0]+space+boardArray[i][1]+space+boardArray[i][2]+space+boardArray[i][3]+space+boardArray[i][4]);
-				}
-				/*for(int i=1;i<boardArray.length;i++) {
-					if(boardArray[i-1][0]==null)
+				} 
+				/*for(int i=100-test-1;i>=0;i--) {
+					if(boardArray[i][0]==null) {
 						continue;
-					if(Integer.parseInt(boardArray[i-1][0])>(Integer.parseInt(boardArray[i][0]))) {
 
-						System.out.println(boardArray[i-1][0]+space+boardArray[i-1][1]+space+boardArray[i-1][2]+space+boardArray[i-1][3]+space+boardArray[i-1][4]);
-					}else {
-						
-						System.out.println(boardArray[100-i][0]+space+boardArray[100-i][1]+space+boardArray[100-i][2]+space+boardArray[100-i][3]+space+boardArray[100-i][4]);
+					}else if(boardArray[i][0].equals(String.valueOf(num))) {
+						System.out.println(boardArray[i][0]+space+boardArray[i][1]+space+boardArray[i][2]+space+boardArray[i][3]+space+boardArray[i][4]);
+						num--;
+						i=100-test;
+					}else if(saveRemoveNum==num||boardArray[i][0]==null){
+						for(int j =0;j<removeCount;j++) {
+							if(removeArray[j]==num) {
+								num--;
+							}
+						}
+
 					}
+					else if(i==(boardArray.length-1)&&num!=boardArray.length) {
+						i=100-test;
+						num--;
+					}else if(num==boardArray.length+1) {
+						break;
+					}		
+
 				}*/
+
+
 			}else if(choiceNum==2) {
 
-
 				count--;
-				for(int i=boardArray.length-1;i>=0;i--) {
+				//test--;
+				/*if(flag) {
+					test++;
+				/}*/
+
+				for(int i=0;i<=boardArray.length;i++) {
 
 					if(boardArray[i][0]==null) {
 						//번호
@@ -87,7 +110,7 @@ public class KimMyungHwi {
 
 				}
 
-
+				//flag=false;
 			}
 
 
@@ -95,11 +118,21 @@ public class KimMyungHwi {
 				System.out.print("번호 : ");
 				String findNum = scanner.nextLine();
 				int findIntNum = Integer.parseInt(findNum);
-				System.out.println("제목 : "+ boardArray[constNum-findIntNum][1]);
-				System.out.println("내용 : "+ boardArray[constNum-findIntNum][2]);
-				System.out.println("글쓴이 : "+ boardArray[constNum-findIntNum][3]);
-				boardArray[constNum-findIntNum][4]=String.valueOf(Integer.parseInt(boardArray[constNum-findIntNum][4])+1);
-				System.out.println("조회수 : "+ boardArray[constNum-findIntNum][4]);
+				int readIndex=-1;
+				for(int i=0; i<boardArray.length;i++) {
+					if(boardArray[i][0]!=null&&boardArray[i][0].equals(String.valueOf(findIntNum))) {
+						readIndex=i;
+					}
+				}
+				if(readIndex==-1) {
+					System.out.println("없는 게시판 번호입니다.");
+				}else {
+					System.out.println("제목 : "+ boardArray[readIndex][1]);
+					System.out.println("내용 : "+ boardArray[readIndex][2]);
+					System.out.println("글쓴이 : "+ boardArray[readIndex][3]);
+					boardArray[readIndex][4]=String.valueOf(Integer.parseInt(boardArray[readIndex][4])+1);
+					System.out.println("조회수 : "+ boardArray[readIndex][4]);
+				}
 			}
 
 
@@ -107,34 +140,63 @@ public class KimMyungHwi {
 				System.out.print("번호 : ");
 				String findNum = scanner.nextLine();
 				int findIntNum = Integer.parseInt(findNum);
-				System.out.println("기존제목 : "+ boardArray[constNum-findIntNum][1]);
-				System.out.print("수정제목 : ");
-				String fix=scanner.nextLine();
-				if(!fix.equals("")) {
-					boardArray[constNum-findIntNum][1]=fix;
+				int changeIndex = -1;
+				for(int i=0; i<boardArray.length;i++) {
+					if(boardArray[i][0]!=null&&boardArray[i][0].equals(String.valueOf(findIntNum))) {
+						changeIndex=i;
+					}
 				}
-				System.out.println("기존내용 : "+ boardArray[constNum-findIntNum][2]);
-				System.out.println("수정내용 : ");
-				String contentsFix = scanner.nextLine();
-				if(!contentsFix.equals("")) {
-					boardArray[constNum-findIntNum][2]=contentsFix;
-					auto=true;
+				if(changeIndex==-1) {
+					System.out.println("없는 게시판 번호입니다.");
 				}else {
-					auto=true;
+					System.out.println("기존제목 : "+ boardArray[changeIndex][1]);
+					System.out.print("수정제목 : ");
+					String fix=scanner.nextLine();
+					if(!fix.equals("")) {
+						boardArray[changeIndex][1]=fix;
+					}
+					System.out.println("기존내용 : "+ boardArray[changeIndex][2]);
+					System.out.println("수정내용 : ");
+					String contentsFix = scanner.nextLine();
+					if(!contentsFix.equals("")) {
+						boardArray[changeIndex][2]=contentsFix;
+						auto=true;
+					}else {
+						auto=true;
+					}
 				}
-			}else if(choiceNum==5) {
+			}
+
+			else if(choiceNum==5) {
 				System.out.print("번호 : ");
 				String removeNum = scanner.nextLine();
 				int removeIntNum = Integer.parseInt(removeNum);
-				for(int i=0; i<5; i++) {
-					boardArray[constNum-removeIntNum][i]=null;
+				saveRemoveNum=removeIntNum;
+				//removeArray[removeCount]=saveRemoveNum;
+				//removeCount++;
+
+				int removeIndex=-1;
+				for(int i=0; i<boardArray.length;i++) {
+					if(boardArray[i][0]!=null&&boardArray[i][0].equals(String.valueOf(removeIntNum))) {
+						removeIndex=i;
+					}
 				}
-				auto=true;
+				if(removeIndex==-1) {
+					System.out.println("없는 게시판 번호입니다.");
+				}else {
+
+					for(int i=0; i<5; i++) {
+						boardArray[removeIndex][i]=null;
+					}
+					auto=true;
+					//flag=true;
+				}
+			}
+			else if(choiceNum ==6) {
+				System.out.println("종료합니다");
+				run=false;
 			}
 
 		}
 	}
 }
-
-
-
